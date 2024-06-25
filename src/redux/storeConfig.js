991989e39f39c35decd066366bf2cf8aa89b69ac
Laura Pérez Betarte <lmperezbetarte@gmail.com>
2024-06-25 10:06:03 -0300

    implementación de persistencia

--- src/redux/storeConfig.js
+++ src/redux/storeConfig.js
@@ -1,9 +1,35 @@
 import userReducer from "./userSlice";
 import tweetsReducer from "./tweetsSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer,
+});
+
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: { user: userReducer, tweets: tweetsReducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export default store;
